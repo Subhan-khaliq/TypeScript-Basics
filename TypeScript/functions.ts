f39d99aaabcfb43b06e12b4accb27d9236641689
Subhan Khaliq <subhankhaliq420@gmail.com>
2022-10-06 13:50:46 +0500

    New Changes

--- TypeScript/functions.ts
+++ TypeScript/functions.ts
@@ -3,3 +3,17 @@ function getValue(val: string): string {
 }
 
 console.log(getValue("Subhan Khaliq"));
+
+
+function evenOdd(val: number, len?: number): boolean {
+  if(val % 2 == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+console.log(evenOdd(15));
